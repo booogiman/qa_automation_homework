382411219a9cd0a9cf4e31364829aa6aea6479d1
Lev Sevko <booogiman@yandex.ru>
2020-09-08 16:32:09 +0300

    третие дз

---
+++ les6/TrigonometricFigures.py
@@ -0,0 +1,139 @@
+import math
+
+
+class TrigFigure:
+    def __init__(self, side1):
+        self.side1 = side1
+        # self.side2 = side2
+        # self.side3 = side3
+
+    _corner = None
+    _name = ""
+
+    def calculatio(self):
+        s = None
+        return s
+
+    def area(self):
+        print(f"Площадь {self._name}а = {self.calculatio()} ")
+        return self.calculatio()
+
+    def nameTrig(self):
+        return self._name
+
+    def angels(self):
+        print(f"Количество углов в данном {self._name} - {self._corner}")
+        return self._corner
+
+    def perimetr(self):
+        if self._corner == 0:
+            sum = 2 * math.pi * self.side1
+        elif self._corner == 3:
+            sum = 0
+            for i in self.side1:
+                sum = sum + i
+        elif self._corner == 4 and len(self.side1) == 2:
+            sum = (self.side1[0] + self.side1[1]) * 2
+        elif self._corner == 4 and len(self.side1) != 2:
+            sum = self.side1[0] * 4
+        else:
+            print("Не верная фигура")
+        print(f"Периметр {self._name}а = {sum}")
+        return sum
+
+    def print(self):
+        self.perimetr()
+        self.area()
+        self.angels()
+
+    def add_square(self, Figur2):
+        # print("тест1")
+        # print(issubclass(Figur2, TrigFigure))
+        # print(Figur2.set_area()+self.calculatio())
+        test_list = (Triangl, Square, Rectangl, Circle)
+        # print(Triangl)
+        # print(test_list)
+        # print(type(test_list))
+        # print(Figur2.calculatio())
+        if isinstance(Figur2, test_list):
+            sum_square = Figur2.calculatio() + self.calculatio()
+            test_str = print(f"Сумма площадей фигур: {Figur2._name} и {self._name} = {sum_square}")
+        else:
+            test_str = print(f"Переданная вами фигура не входит в соства допустимых фигур")
+        return sum_square
+
+
+# test = TrigFigure(1)
+# test.add_square()
+
+class Triangl(TrigFigure):
+    def __init__(self, side1):
+        super().__init__(side1)
+
+    _corner = 3
+    _name = "Триугольник"
+
+    def calculatio(self):
+        p = (self.side1[0] + self.side1[1] + self.side1[2]) / 2
+        s = math.sqrt(p * (p - self.side1[0]) * (p - self.side1[1]) * (p - self.side1[2]))
+        return s
+
+
+class Rectangl(TrigFigure):
+    def __init__(self, side1):
+        super().__init__(side1)
+
+    _corner = 4
+    _name = "Прямоугольник"
+
+    def calculatio(self):
+        s = self.side1[0] * self.side1[1]
+        return s
+    # def perimetr(self):
+    #     perim = (self.side1[0]+self.side1[1]) * 2
+    #     print(f"Периметр {self._name}а = {perim}")
+
+
+class Square(TrigFigure):
+    def __init__(self, side1):
+        super().__init__(side1)
+
+    _corner = 4
+    _name = "Квадрат"
+
+    def calculatio(self):
+        s = self.side1 * self.side1
+        # s = self.side1 * self.side1
+        return s
+
+
+class Circle(TrigFigure):
+    def __init__(self, side1):
+        super().__init__(side1)
+
+    _corner = 0
+    _name = "Круг"
+
+    def calculatio(self):
+        s = self.side1 * self.side1 * math.pi
+        return s
+
+# class Car:
+#     pass
+#
+#
+# circ1 = Circle([3])
+# circ1.print()
+# square1 = Square([5])
+# square1.print()
+# square1.add_square(circ1)
+# square1.add_square(Car)
+# # print(issubclass(circ1, Circle))
+# # print(isinstance(circ1,Circle))
+# # print(issubclass(Circle,TrigFigure))
+# # print(issubclass(circ1,TrigFigure))
+# # triangl1 = Triangl([3,3,3])
+# # triangl1.print()
+# #
+# # rectangl1 = Rectangl([2,3])
+# # rectangl1.print()
--- lesso6-7_new/test/localTest.py
+++ lesso6-7_new/test/localTest.py
@@ -1,31 +1,33 @@
-import pytest,math
+import pytest, math
 
-from homework.lesson3.TrigonometricFigures import Triangl,Rectangl,Square,Circle
+from homework.les6.TrigonometricFigures import Triangl, Rectangl, Square, Circle
 
-class TestsTriangl:
 
-    @pytest.mark.parametrize("test_vars", argvalues=[(8,18,12),(15,25,13),(8,9,14)])
-    def test_area (self, test_vars):
-        side1 = test_vars
-        area = Triangl(test_vars)
-        p = (test_vars[0] + test_vars[1] + test_vars[2])/2
-        test_area = math.sqrt(p*(p-test_vars[0])*(p-test_vars[1])*(p-test_vars[2]))
-        if area.area() != test_area:
-            print(f"Тестируемое число{area.area()}, сравнивается с расчетным {test_area}. Тест не пройден")
-        assert area.area() == test_area
-    @pytest.mark.parametrize("test_name_list", ["Триугольник","Прямоугольник","Квадрат","Круг"])
-    def test_name (self, test_name_list):
-        name = Triangl((8,18,12))
-        name.print()
-        assert name.nameTrig() == test_name_list
-
-    @pytest.mark.parametrize("test_angels", [0,1,2,3,4,5,6])
-    def test_angels(self,test_angels):
-        test_triangls = Triangl((8,18,12))
-        assert test_triangls.angels() == test_angels
+class TestsTriangl1:
 
     @pytest.mark.parametrize("test_vars", argvalues=[(8, 18, 12), (15, 25, 13), (8, 9, 14)])
     def test_area(self, test_vars):
+        side1 = test_vars
+        area = Triangl(test_vars)
+        p = (test_vars[0] + test_vars[1] + test_vars[2]) / 2
+        test_area = math.sqrt(p * (p - test_vars[0]) * (p - test_vars[1]) * (p - test_vars[2]))
+        if area.area() != test_area:
+            print(f"Тестируемое число{area.area()}, сравнивается с расчетным {test_area}. Тест не пройден")
+        assert area.area() == test_area
+
+    @pytest.mark.parametrize("test_name_list", ["Триугольник", "Прямоугольник", "Квадрат", "Круг"])
+    def test_name(self, test_name_list):
+        name = Triangl((8, 18, 12))
+        name.print()
+        assert name.nameTrig() == test_name_list
+
+    @pytest.mark.parametrize("test_angels", [0, 1, 2, 3, 4, 5, 6])
+    def test_angels(self, test_angels):
+        test_triangls = Triangl((8, 18, 12))
+        assert test_triangls.angels() == test_angels
+
+    @pytest.mark.parametrize("test_vars", argvalues=[(8, 18, 12), (15, 25, 13), (8, 9, 14)])
+    def test_perimetr(self, test_vars):
         side1 = test_vars
         area = Triangl(test_vars)
         p = test_vars[0] + test_vars[1] + test_vars[2]
@@ -33,16 +35,123 @@ class TestsTriangl:
             print(f"Тестируемое число{area.perimetr()}, сравнивается с расчетным {p}. Тест не пройден")
         assert area.perimetr() == p
 
-    # @pytest.mark.parametrize("test_triangl", [(8, 18, 12), (15, 25, 13), (8, 9, 14)])
-    # @pytest.mark.parametrize("test_square", [2,3,4,5,6])
-    # def test_sum_area(self,test_triangl,test_square):
-    #     first_triangl = create_triangl(test_triangl)
-    #     first_triangl.print()
-    def test_sum_area(self,create_triangl,create_square):
-        # create_triangl.area()
-        # create_square.area()
-        sum = create_triangl.calculatio() + create_square.calculatio()
-        print(sum)
-        print(create_triangl.add_square(create_square))
-        # assert sum == create_square.add_square(create_triangl)
+    @pytest.mark.parametrize("test_triangl", [(8, 18, 12), (15, 25, 13), (8, 9, 14)])
+    @pytest.mark.parametrize("test_rect", [(1,2), (4,4), (9,7), (10,15), (9,9)])
+    def test_sum_area(self, test_triangl, test_rect):
+        triangl = Triangl(test_triangl)
+        rect = Rectangl(test_rect)
+        sum = triangl.calculatio() + rect.calculatio()
+        assert sum == triangl.add_square(rect)
 
+class TestsRectangl1:
+
+    @pytest.mark.parametrize("test_vars", argvalues=[(8, 18), (15, 25), (8, 9)])
+    def test_area(self, test_vars):
+        side1 = test_vars
+        area = Rectangl(test_vars)
+        test_area = test_vars[0]*test_vars[1]
+        if area.area() != test_area:
+            print(f"Тестируемое число{area.area()}, сравнивается с расчетным {test_area}. Тест не пройден")
+        assert area.area() == test_area
+
+    @pytest.mark.parametrize("test_name_list", ["Триугольник", "Прямоугольник", "Квадрат", "Круг"])
+    def test_name(self, test_name_list):
+        name = Rectangl((8, 18))
+        name.print()
+        assert name.nameTrig() == test_name_list
+
+    @pytest.mark.parametrize("test_angels", [0, 1, 2, 3, 4, 5, 6])
+    def test_angels(self, test_angels):
+        test_rect = Rectangl((8, 18))
+        assert test_rect.angels() == test_angels
+
+    @pytest.mark.parametrize("test_vars", argvalues=[(8, 18), (15, 25), (8, 9)])
+    def test_perimetr(self, test_vars):
+        side1 =Rectangl(test_vars)
+        p = (test_vars[0] + test_vars[1])*2
+        if side1.perimetr() != p:
+            print(f"Тестируемое число{side1.perimetr()}, сравнивается с расчетным {p}. Тест не пройден")
+        assert side1.perimetr() == p
+
+    @pytest.mark.parametrize("test_triangl", [(8, 18, 12), (15, 25, 13), (8, 9, 14)])
+    @pytest.mark.parametrize("test_rect", [(1,2), (4,4), (9,7), (10,15), (9,9)])
+    def test_sum_area(self, test_triangl, test_rect):
+        triangl = Triangl(test_triangl)
+        rect = Rectangl(test_rect)
+        sum = rect.calculatio() + triangl.calculatio()
+        assert sum == rect.add_square(triangl)
+
+class TestsSquare1:
+
+    @pytest.mark.parametrize("test_vars", argvalues=[18, 17, 32])
+    def test_area(self, test_vars):
+        area = Square(test_vars)
+        test_area = test_vars*test_vars
+        if area.area() != test_area:
+            print(f"Тестируемое число{area.area()}, сравнивается с расчетным {test_area}. Тест не пройден")
+        assert area.area() == test_area
+
+    @pytest.mark.parametrize("test_name_list", ["Триугольник", "Прямоугольник", "Квадрат", "Круг"])
+    def test_name(self, test_name_list):
+        name = Square(20)
+        name.print()
+        assert name.nameTrig() == test_name_list
+
+    @pytest.mark.parametrize("test_angels", [0, 1, 2, 3, 4, 5, 6])
+    def test_angels(self, test_angels):
+        test_rect = Square(12)
+        assert test_rect.angels() == test_angels
+
+    @pytest.mark.parametrize("test_vars", argvalues=[(8, 18), (15, 25), (8, 9)])
+    def test_perimetr(self, test_vars):
+        side1 =Rectangl(test_vars)
+        p = (test_vars[0] + test_vars[1])*2
+        if side1.perimetr() != p:
+            print(f"Тестируемое число{side1.perimetr()}, сравнивается с расчетным {p}. Тест не пройден")
+        assert side1.perimetr() == p
+
+    @pytest.mark.parametrize("test_triangl", [(8, 18, 12), (15, 25, 13), (8, 9, 14)])
+    @pytest.mark.parametrize("test_square", [20, 35, 17, 22, 34])
+    def test_sum_area(self, test_triangl, test_square):
+        triangl = Triangl(test_triangl)
+        square = Square(test_square)
+        sum = square.calculatio() + triangl.calculatio()
+        assert sum == square.add_square(triangl)
+
+
+class TestsCircl1:
+
+    @pytest.mark.parametrize("test_vars", argvalues=[8, 15, 8])
+    def test_area(self, test_vars):
+        area = Circle(test_vars)
+        test_area = test_vars*test_vars*math.pi
+        if area.area() != test_area:
+            print(f"Тестируемое число{area.area()}, сравнивается с расчетным {test_area}. Тест не пройден")
+        print(area.nameTrig())
+        assert area.area() == test_area
+
+    @pytest.mark.parametrize("test_name_list", ["Триугольник", "Прямоугольник", "Квадрат", "Круг"])
+    def test_name(self, test_name_list):
+        name = Circle(8)
+        assert name.nameTrig() == test_name_list
+
+    @pytest.mark.parametrize("test_angels", [0, 1, 2, 3, 4, 5, 6])
+    def test_angels(self, test_angels):
+        test_rect = Circle(8)
+        assert test_rect.angels() == test_angels
+
+    @pytest.mark.parametrize("test_vars", argvalues=[8, 25, 10])
+    def test_perimetr(self, test_vars):
+        side1 =Circle(test_vars)
+        p = 2 * math.pi * test_vars
+        if side1.perimetr() != p:
+            print(f"Тестируемое число{side1.perimetr()}, сравнивается с расчетным {p}. Тест не пройден")
+        assert side1.perimetr() == p
+
+    @pytest.mark.parametrize("test_circl", [3, 5, 6, 7, 9, 2])
+    @pytest.mark.parametrize("test_rect", [(1,2), (4,4), (9,7), (10,15), (9,9)])
+    def test_sum_area(self, test_circl, test_rect):
+        circl = Circle(test_circl)
+        rect = Rectangl(test_rect)
+        sum = rect.calculatio() + circl.calculatio()
+        assert sum == circl.add_square(rect)
